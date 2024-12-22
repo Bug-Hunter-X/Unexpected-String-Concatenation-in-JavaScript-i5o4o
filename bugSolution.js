78948e71bc8c57ecad19676181227fdebf0ce6e9
BugHunter-X <askzenai@gmail.com>
2024-12-22 01:06:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+function myFunction(a, b) {
+  return a + Number(b);
+}
+
+console.log(myFunction(5, '5')); // Output: 10 (numerical addition) 
+
+function myFunction2(a,b){
+    return a + parseInt(b);
+}
+console.log(myFunction2(5,'5')); //Output: 10
